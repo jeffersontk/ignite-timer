ff80a8da6045c8252da34d2ea85494b53b6cdfa1
Jefferson Silva Brito Cabral <jscabral@firjan.com.br>
2022-07-28 22:08:23 -0300

    criando layout da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { Router } from './Router'
+import { ThemeProvider } from 'styled-components'
+import { defaultThemes } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
 export function App() {
-  return <h1>hello world </h1>
+  return (
+    <ThemeProvider theme={defaultThemes}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import { ThemeProvider } from 'styled-components'
-import { defaultThemes } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={defaultThemes}>
-      <App />
-      <GlobalStyle />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
 )
